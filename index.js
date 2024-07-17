ff7437d0e4b9b06a6a4c1aae8a760616120fc0c9
miatia1025 <miatia1025@gmail.com>
2024-07-17 23:21:27 +0900

    huxn mongodb completed"

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import {
+  createDoc,
+  insertManyDoc,
+  singleDoc,
+  allDoc,
+  docWithField,
+  updateById,
+} from "./models/Movies.js";
+
 import { connectDB } from "./db/connectDB.js";
 
 const app = express();
@@ -6,5 +15,8 @@ const port = process.env.PORT || 8000;
 const DATABASE_URL = process.env.DATABASE_URL || `mongodb://127.0.0.1:27017/`;
 
 connectDB(DATABASE_URL);
+
+// Test
+updateById("6697cf0ebfcf1c771b5235e2");
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
--- models/Movies.js
+++ models/Movies.js
@@ -14,3 +14,118 @@ const movieSchema = new mongoose.Schema({
     { value: { type: String }, published: { type: Date, default: Date.now } },
   ],
 });
+
+const MovieModel = mongoose.model("Movie", movieSchema);
+
+export const createDoc = async () => {
+  try {
+    const m1 = new MovieModel({
+      name: "Extraction 2",
+      ratings: 4,
+      money: 60000,
+      genre: ["action", "adventure"],
+      isActive: true,
+      comments: [{ value: "That was an amazing movie." }],
+    });
+    const result = await m1.save();
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const insertManyDoc = async () => {
+  try {
+    const m1 = new MovieModel({
+      name: "Extraction 2",
+      ratings: 4,
+      money: 60000,
+      genre: ["action", "adventure"],
+      isActive: true,
+      comments: [{ value: "That was an amazing movie." }],
+    });
+    const m2 = new MovieModel({
+      name: "John Wick 4",
+      ratings: 4,
+      money: 23000,
+      genre: ["action"],
+      isActive: true,
+      comments: [{ value: "John doesn't seem that angry any more:(" }],
+    });
+    const m3 = new MovieModel({
+      name: "Mission Impossible - Dead Reckoning Part One",
+      ratings: 4,
+      money: 60000,
+      genre: ["action", "spy", "crime film", "thriller"],
+      isActive: true,
+      comments: [{ value: "Ok that was TOM but where is Jeryy." }],
+    });
+    const m4 = new MovieModel({
+      name: "Transformers",
+      ratings: 4,
+      money: 220000,
+      genre: ["action", "adventure", "Science Fiction", "fantasy"],
+      isActive: true,
+      comments: [{ value: "That was enough VFX for today." }],
+    });
+    const m5 = new MovieModel({
+      name: "The Expendables 4",
+      ratings: 4,
+      money: 250000,
+      genre: ["action", "war", "comedy", "thriller"],
+      isActive: true,
+      comments: [
+        { value: "That was enough fighting and blowing stuff for today." },
+      ],
+    });
+    const result = await MovieModel.insertMany([m1, m2, m3, m4, m5]);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const allDoc = async () => {
+  try {
+    const result = await MovieModel.find();
+    console.clear();
+    result.forEach((movie) => {
+      console.log(movie.name);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const singleDoc = async () => {
+  try {
+    const result = await MovieModel.findById("6697cf0ebfcf1c771b5235de");
+    console.clear();
+    console.log(result.name);
+    console.log(result.ratings);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const docWithField = async () => {
+  try {
+    const result = await MovieModel.find({
+      $and: [{ money: 220000 }, { ratings: 4 }],
+    });
+    console.clear();
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const updateById = async (id) => {
+  try {
+    const result = await MovieModel.updateOne({ _id: id }, { ratings: 5 });
+    console.clear();
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+};
