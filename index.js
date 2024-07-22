15718bfbfec7604b0843842271c636168a41d122
miatia1025 <miatia1025@gmail.com>
2024-07-22 20:25:18 +0900

    periodically commit

--- index.js
+++ index.js
@@ -17,6 +17,5 @@ const DATABASE_URL = process.env.DATABASE_URL || `mongodb://127.0.0.1:27017/`;
 connectDB(DATABASE_URL);
 
 // Test
-updateById("6697cf0ebfcf1c771b5235e2");
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
